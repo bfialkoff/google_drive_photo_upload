706ce639d66d276fa0fd561aa7bec0a3b275ff70
bfialkoff <bfialkoff@gmail.com>
2020-11-29 19:58:40 +0200

    pushing to heroku

---
+++ settings/telegram_credentials.py
@@ -0,0 +1,6 @@
+url_host = 'https://93792fa47b3d.ngrok.io'
+telegram_token = '1156639980:AAH59Yi1udq15JrUlxKyUcsOB7l9-a6DjnA'
+telegram_set_webhook = 'api.telegram.org/bot{}/setWebHook?url={}'.format(telegram_token, url_host)
+telegream_webhook_info = 'api.telegram.org/bot{}/getWebhookInfo'.format(telegram_token)
+bot_url = 'https://api.telegram.org/bot{}/'.format(telegram_token)
+
